764edca1d6565c3275a5321213e1743a74cc2ef1
TARINunit9 <35309446+TARINunit9@users.noreply.github.com>
2018-01-19 11:23:23 -0800

    Create SourceTwo.cpp
    Tarin here
    Just putting our includes and main function, let's actually make this thing

---
+++ SourceTwo.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstring>
+#include<iomanip>
+#include"puzzle.h"
+#include"game.h"
+using namespace std;
+
+int main()
+{
+	while(isPlaying){
+  
+  }
+  return 0;
+}
