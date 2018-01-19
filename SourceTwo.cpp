268c154d92cf2ee2234bd9a1148f3f29e5f46a41
TARINunit9 <35309446+TARINunit9@users.noreply.github.com>
2018-01-19 11:41:53 -0800

    Update SourceTwo.cpp

--- SourceTwo.cpp
+++ SourceTwo.cpp
@@ -4,13 +4,27 @@
 #include<cstring>
 #include<iomanip>
 #include"menu.h"
+#include"nonogram.h"
 using namespace std;
 
 int main(){
 	
-	//menu menu = new menu.menuSys();
-	//while(menu.isPlaying()){
-	
- 	//}
+	menu menu;			//declare out menu
+	menu.menuSys();			//call the constructor
+	while(menu.isPlaying()){	//so long as the user wants to play our game we stay here
+		//now we are at level one, asking the user what they would like to do to start
+		//besides exit, their option is to selevt a game
+		
+		//now we are at level two
+		//besides exit, their option is to select a puzzle file
+		//they may specify a puzzle file, or default to a default puzzle
+		
+		//now we are at level three: playing the game
+		//their options are to make a move, or open a userMenu
+		
+		//userMenu level
+		//their options are to exit the game, or request help
+		//if possible we can add a function later to return to clear the puzzle and level one
+ 	}
 	return 0;
 }
