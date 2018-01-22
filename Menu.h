c27f179d2819a1c62902d940c92a414d6a55adba
qk8333 <evan.smith.m@gmail.com>
2018-01-22 14:15:05 -0800

    Update Menu.h
    * Note: In coding, it is common etiquette to have all classes capitalized so I changed the menuSys class to Menu for simplicity.
    * You have some functions that don't seem to do anything such as the readString() function which I commented out.
    * I would recommend adding an enum variable to store game types rather than just using integers.
    * You should use while-loops for type checking to make sure the user entered the proper variable. Have them loop back if an improper command is entered.
    * Don't worry about an isWin() function as they will already be part of the Game classes. You can put it in an indefinite loop.

--- Menu.h
+++ Menu.h
@@ -7,19 +7,21 @@
 #include <cstring>
 using namespace std;
 
-class menuSys	//menu class for user traversal
+class Menu	//menu class for user traversal
 {
 
 public:
-	menuSys();	//standard constructor
-	~menuSys();	//standard destructor
+	Menu();	//standard constructor
+	~Menu();	//standard destructor
 	void selectGame();	//sets which game is being played
 	void selectPuzzle();	//selects a specific puzzle
 	void makeMove();	//makes a move
 	void userMenu();	//the user invokes this if they want to open a help menu, or exit the program during a puzzle
 	bool isWin(); //checks if the game is won
 	bool isPlaying(); //checks if the user still wants to play
-	void readString(string a);	//reads a string and acts accordingly
+	/*
+	void readString(string);	//reads a string and acts accordingly
+	*/
 private:
 	string puzzle;	//stores puzzle name
 	int userMenuType; 	//stores whatever the user types in userMenu
@@ -28,7 +30,7 @@ private:
 	bool isPlaying;	//used to track if the user is still wanting to play
 };
 
-void menuSys::selectGame()
+void Menu::selectGame()
 {
 	int a;
 	cout << "Please pick a number between 1 and 2:" << endl << "1=picross   2=rullo" << endl << "Or type 9 to exit." << endl;
@@ -54,31 +56,34 @@ void menuSys::selectGame()
 	}
 	game = a;
 };
-void menuSys::selectPuzzle()
+void Menu::selectPuzzle()
 {
 	cout << "Now, please select a puzzle name: ";
 	cin >> puzzle;
 	cout << endl << "thanks! you will open " << puzzle << "!" << endl;
 	
 };
-void menuSys::readString(string a)
+/*
+void Menu::readString(string str) 
 {
-	string buf = a;
+	string buf = str;
 	// buf.c_str()
 };
-void menuSys::makeMove()
+*/
+
+void Menu::makeMove()
 {
 
 };
-bool menuSys::isWin()
+bool Menu::isWin()
 {
 
 };
-bool menuSys::isPlaying()
+bool Menu::isPlaying()
 {
 	
 };
-void menuSys::userMenu()
+void Menu::userMenu()
 {
 	cout << "Hello there. What would you like help with?" << endl;
 	cout << "If you would like instructions, type 1. If you would like to exit our game, type 9. Otherwise, type 5 to return to your game." << endl;
@@ -111,13 +116,13 @@ void menuSys::userMenu()
 			break; //yes, this is the same as case 5
 	}
 };
-menuSys::menuSys()
+Menu::Menu()
 {
-menuSys.selectGame();
-menuSys.selectPuzzle();
-menuSys.isPlaying=TRUE;
+this.selectGame();
+this.selectPuzzle();
+this.isPlaying=TRUE;
 };
-menuSys::~menuSys()
+Menu::~Menu()
 {
 
 };
