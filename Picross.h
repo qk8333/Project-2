848b8b1541cd21c2ecc2f02a1870b8f1b684bb06
qk8333 <evan.smith.m@gmail.com>
2018-01-24 19:39:05 -0800

    Update Picross.h

--- Picross.h
+++ Picross.h
@@ -6,6 +6,9 @@
 #include<string>
 using namespace std;
 
+#include<cstdlib>
+#include<ctime>
+
 #include "Game.h"
 
 class Picross : public Game
@@ -22,142 +25,39 @@ public:
 	virtual int getColKey(int row, int col) const { return Game::getColKey(row, col); }
 	virtual Space * getSolution(int row, int col) const { return Game::getSolution(row, col); }
 	virtual Space * getBoard(int row, int col) const { return Game::getBoard(row, col); }
-	virtual bool isWin() const;
-	virtual void move(int row, int col);
+	virtual bool isWin() const { return Game::isWin(); }
+	virtual void move(int row, int col, int val = 0) { Game::move(row, col, 0); }
 	virtual void createGame(Space**, int, int);
 	virtual void loadGame(string);
+	virtual void saveGame(string filename) { Game::saveGame(filename); }
 	virtual ~Picross() { Game::~Game(); }
 private:
-	void createKeys();
+	virtual void generateKeys();
 };
 
-Picross::Picross(string filename)
+Picross::Picross(string filename) : Game(filename)
 {
-	ifstream file;
-	file.open(filename);
-	if (!file.good()) numRows = 0, numCols = 0, solution = nullptr, board = nullptr, rowKey = nullptr, colKey = nullptr;
-	else
-	{
-		file >> numRows;
-		file >> numCols;
-		board = new Space*[getNumRows()];
-		solution = new Space*[getNumRows()];
-		for (int row = 0; row < getNumRows(); row++)
-		{
-			board[row] = new Space[getNumCols()];
-			solution[row] = new Space[getNumCols()];
-			for (int col = 0; col < getNumCols(); col++)
-			{
-				board[row][col].integer = 0;
-				board[row][col].boolean = false;
-				solution[row][col].integer = 0;
-				solution[row][col].boolean = false;
-			}
-		}
-		int row, col;
-		while (!file.eof())
-		{
-			file >> row;
-			file >> col;
-			solution[row][col].boolean = true;
-		}
-		createKeys();
-	}
-	file.close();
+	generateKeys();
 }
 
 Picross::Picross(Space **sol, int row, int col) : Game(sol, row, col)
 {
-	board = new Space*[getNumRows()];
-	for (int i = 0; i < getNumRows(); i++)
-	{
-		board[i] = new Space[getNumCols()];
-		for (int j = 0; j < getNumCols(); j++)
-		{
-			board[i][j].integer = 0;
-			board[i][j].boolean = false;
-		}
-	}
-	createKeys();
-}
-
-bool Picross::isWin() const
-{
-	for (int row = 0; row < getNumRows(); row++)
-	{
-		for (int col = 0; col < getNumCols(); col++)
-		{
-			if (board[row][col].boolean != solution[row][col].boolean) return false; else;
-		}
-	}
-	return true;
-}
-
-void Picross::move(int row, int col)
-{
-	if (row < 0 || row >= getNumRows() || col < 0 || col >= getNumCols()) return;
-	else board[row][col].boolean = !board[row][col].boolean;
+	generateKeys();
 }
 
 void Picross::createGame(Space** sol, int row, int col)
 {
-	for (int i = 0; i < numRows; i++)
-	{
-		delete[] rowKey[i];
-		delete[] solution[i];
-		delete[] board[i];
-	}
-	for (int i = 0; i < getColKeyHeight(); i++) delete[] colKey[i];
-	solution = sol, numRows = row, numCols = col;
-	board = new Space*[getNumRows()];
-	for (int i = 0; i < getNumRows(); i++)
-	{
-		board[i] = new Space[getNumCols()];
-		for (int j = 0; j < getNumCols(); j++)
-		{
-			board[i][j].integer = 0;
-			board[i][j].boolean = false;
-		}
-	}
-	createKeys();
+	Game::createGame(sol, row, col);
+	generateKeys();
 }
 
 void Picross::loadGame(string filename)
 {
-	ifstream file;
-	file.open(filename);
-	if (!file.good()) return;
-	else
-	{
-		file >> numRows;
-		file >> numCols;
-		board = new Space*[getNumRows()];
-		solution = new Space*[getNumRows()];
-		for (int row = 0; row < getNumRows(); row++)
-		{
-			board[row] = new Space[getNumCols()];
-			solution[row] = new Space[getNumCols()];
-			for (int col = 0; col < getNumCols(); col++)
-			{
-				board[row][col].integer = 0;
-				board[row][col].boolean = false;
-				solution[row][col].integer = 0;
-				solution[row][col].boolean = false;
-			}
-		}
-		int row, col;
-		while (!file.eof())
-		{
-			file >> row;
-			file >> col;
-			solution[row][col].boolean = true;
-		}
-		createKeys();
-		file.close();
-	}
+	Game::loadGame(filename);
+	generateKeys();
 }
 
-void Picross::createKeys()
+void Picross::generateKeys()
 {
 	rowKey = new int*[getNumRows()];
 	for (int i = 0; i < getNumRows(); i++)
